78010869cd1702c7a6c0fd5e12058cba461dfd1d
Voiner Eugene <no2limit25@gmail.com>
2026-02-02 17:13:10 +1000

    new file:   .editorconfig 	new file:   .env 	new file:   .env.dev 	new file:   .env.test 	new file:   .gitignore 	new file:   .idea/.gitignore 	new file:   .idea/commandlinetools/Symfony_28_01_2026__16_48.xml 	new file:   .idea/commandlinetools/schemas/frameworkDescriptionVersion1.1.4.xsd 	new file:   .idea/laravel-idea.xml 	new file:   .idea/modules.xml 	new file:   .idea/php.xml 	new file:   .idea/to-do list.iml 	new file:   .idea/vcs.xml 	new file:   assets/app.js 	new file:   assets/controllers.json 	new file:   assets/controllers/csrf_protection_controller.js 	new file:   assets/controllers/hello_controller.js 	new file:   assets/stimulus_bootstrap.js 	new file:   assets/styles/app.css 	new file:   bin/console 	new file:   bin/phpunit 	new file:   compose.override.yaml 	new file:   compose.yaml 	new file:   composer.json 	new file:   composer.lock 	new file:   config/bundles.php 	new file:   config/packages/asset_mapper.yaml 	new file:   config/packages/cache.yaml 	new file:   config/packages/csrf.yaml 	new file:   config/packages/debug.yaml 	new file:   config/packages/doctrine.yaml 	new file:   config/packages/doctrine_migrations.yaml 	new file:   config/packages/framework.yaml 	new file:   config/packages/mailer.yaml 	new file:   config/packages/messenger.yaml 	new file:   config/packages/monolog.yaml 	new file:   config/packages/notifier.yaml 	new file:   config/packages/property_info.yaml 	new file:   config/packages/routing.yaml 	new file:   config/packages/security.yaml 	new file:   config/packages/translation.yaml 	new file:   config/packages/twig.yaml 	new file:   config/packages/ux_turbo.yaml 	new file:   config/packages/validator.yaml 	new file:   config/packages/web_profiler.yaml 	new file:   config/preload.php 	new file:   config/reference.php 	new file:   config/routes.yaml 	new file:   config/routes/framework.yaml 	new file:   config/routes/security.yaml 	new file:   config/routes/web_profiler.yaml 	new file:   config/services.yaml 	new file:   importmap.php 	new file:   migrations/.gitignore 	new file:   migrations/Version20260128072517.php 	new file:   phpunit.dist.xml 	new file:   public/index.php 	new file:   src/Controller/.gitignore 	new file:   src/Entity/.gitignore 	new file:   src/Entity/Task.php 	new file:   src/Kernel.php 	new file:   src/Repository/.gitignore 	new file:   src/Repository/TaskRepository.php 	new file:   symfony.lock 	new file:   templates/base.html.twig 	new file:   tests/bootstrap.php 	new file:   translations/.gitignore

---
+++ src/Entity/Task.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TaskRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TaskRepository::class)]
+class Task
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?bool $isDone = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $updateAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function isDone(): ?bool
+    {
+        return $this->isDone;
+    }
+
+    public function setIsDone(bool $isDone): static
+    {
+        $this->isDone = $isDone;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdateAt(): ?\DateTimeImmutable
+    {
+        return $this->updateAt;
+    }
+
+    public function setUpdateAt(\DateTimeImmutable $updateAt): static
+    {
+        $this->updateAt = $updateAt;
+
+        return $this;
+    }
+}
